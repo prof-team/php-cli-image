2abfaecd4f5bb5448a095685f9707e974177c091
Grischuk Sasha <grischuk.alexandr@gmail.com>
2019-01-22 13:57:38 +0200

    init 7.3

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t grischuksasha/php-cli:7.2 .
-docker push grischuksasha/php-cli:7.2
+docker build -t grischuksasha/php-cli:7.3 .
+docker push grischuksasha/php-cli:7.3
