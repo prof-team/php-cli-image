0c2fd7a1b0dbf433d142fed5bd25eae224ae0047
Green <green@inernetbs.net>
2022-11-21 22:17:23 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,10 @@ if [ -z "$XDEBUG_CONFIG" ]; then
     rm -f /usr/local/etc/php/conf.d/xdebug.ini
 fi
 
+if [ -n "$INI_MEMORY_LIMIT" ]; then
+    sed -i "s/memory_limit.*/memory_limit=$INI_MEMORY_LIMIT/" /usr/local/etc/php/conf.d/common.ini
+fi
+
 if [ -f /custom-entrypoint.sh ]; then
     bash /custom-entrypoint.sh
 fi
